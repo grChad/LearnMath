de48efd81594881518a84291405968a7319b5e71
grChad <bgos-jesus@hotmail.com>
2024-12-13 10:54:48 -0500

    feat: install react-navigation/native and configure

---
+++ src/navigation/index.tsx
@@ -0,0 +1,12 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {View, Text} from 'react-native';
+
+export default function NavigationApp() {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </NavigationContainer>
+  );
+}
