9ae1b89e9472b2ea7d46cf3e5815030e8cf32331
grChad <bgos-jesus@hotmail.com>
2025-01-03 22:25:04 -0500

    chore: delete line unnecessary

--- src/types/store.d.ts
+++ src/types/store.d.ts
@@ -1,5 +1,3 @@
-import { BasicOperationOptions } from '../constants/basicData'
-
 export type BasicOperationType = 'Suma' | 'Resta' | 'Multiplicación' | 'División' | null
 
 export interface BasicState {
