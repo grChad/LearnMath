d6227ab1a5b05e273704ce8385414e3e4545675a
grChad <bgos-jesus@hotmail.com>
2025-01-07 13:45:16 -0500

    feat: update and rename sounds

--- src/screens/BottomTab/basic/index.tsx
+++ src/screens/BottomTab/basic/index.tsx
@@ -6,32 +6,32 @@ import Sound from 'react-native-sound'
 import SelectOperation from './SelectOperation'
 import SelectLevel from './level'
 
-const sound_press_select = new Sound('press.wav', Sound.MAIN_BUNDLE, (error) => {
+const sound_bubble = new Sound('select_bubble.wav', Sound.MAIN_BUNDLE, (error) => {
 	if (error) {
-		throw new Error(`sound_press_select: failed to load the sound ${error}`)
+		throw new Error(`sound_bubble: failed to load the sound ${error}`)
 	}
 })
 
-const sound_press_button = new Sound('press_button.wav', Sound.MAIN_BUNDLE, (error) => {
+const sound_options = new Sound('select_options.wav', Sound.MAIN_BUNDLE, (error) => {
 	if (error) {
-		throw new Error(`sound_press_button: failed to load the sound ${error}`)
+		throw new Error(`sound_options: failed to load the sound ${error}`)
 	}
 })
 
 export default function BasicTabScreen() {
 	const pressButtonSelect = () => {
-		sound_press_select.play((success) => {
+		sound_bubble.play((success) => {
 			if (!success) {
-				throw new Error('sound_press_select: falla el audio')
+				throw new Error('sound_bubble: falla el audio')
 			}
 		})
 	}
 
 	const pressButton = () => {
-		sound_press_button
+		sound_options
 			.play((success) => {
 				if (!success) {
-					throw new Error('sound_press_button: falla el audio')
+					throw new Error('sound_options: falla el audio')
 				}
 			})
 			.setVolume(0.5)
@@ -39,8 +39,8 @@ export default function BasicTabScreen() {
 
 	useEffect(() => {
 		return () => {
-			sound_press_select.release()
-			sound_press_button.release()
+			sound_bubble.release()
+			sound_options.release()
 		}
 	}, [])
 
